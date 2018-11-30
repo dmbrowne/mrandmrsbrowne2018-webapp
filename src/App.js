e53bf421745942647127676ff25e966ec87cf59e
Daryl Browne <daryl.browne@googlemail.com>
2018-11-30 03:24:59 +0000

    add tos and privacy policy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import ConfigureISpy from './pages/ConfigureISpy';
 import Admin from './pages/Admin';
+import TermsOfService from './pages/ToS';
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import IndexPage from './pages/';
 
 function isRunningStandalone() {
@@ -17,14 +19,15 @@ function isRunningOnMobile() {
 
 class App extends Component {
   render() {
-    return (
+    return
       <Switch>
         <Route exact path="/admin" component={Admin} />
         <Route exact path="/games/configure/ispy" component={ConfigureISpy} />
         <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/terms" component={TermsOfService} />
+        <Route exact path="/privacy" component={PrivacyPolicy} />
         <Route path="/" component={IndexPage} />
-      </Switch>
-    );
+      </Switch>;
   }
 }
 
---
+++ src/pages/ToS.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function ToS() {
+  return (
+    <div>
+      <h1>Terms of Service ("Terms")</h1>
+      <p>Last updated: 30/11/2018</p>
+      <p>
+        Please read these Terms of Service ("Terms", "Terms of Service")
+        carefully before using the http://mrandmrsbrowne2018.com website and the
+        Mr and Mrs Browne mobile application (the "Service") operated by Fluxion
+        labs ltd ("us", "we", or "our").
+      </p>
+      <p>
+        Your access to and use of the Service is conditioned on your acceptance
+        of and compliance with these Terms. These Terms apply to all visitors,
+        users and others who access or use the Service.
+      </p>
+      <p>
+        By accessing or using the Service you agree to be bound by these Terms.
+        If you disagree with any part of the terms then you may not access the
+        Service.
+      </p>
+      <h2>Termination</h2>
+      <p>
+        We may terminate or suspend access to our Service immediately, without
+        prior notice or liability, for any reason whatsoever, including without
+        limitation if you breach the Terms.
+      </p>
+      <p>
+        All provisions of the Terms which by their nature should survive
+        termination shall survive termination, including, without limitation,
+        ownership provisions, warranty disclaimers, indemnity and limitations of
+        liability.
+      </p>
+      <h2>Content</h2>
+      <p>
+        Our Service allows you to post, store, share and otherwise make
+        available certain information, text, graphics, videos, or other material
+        ("Content"). You are responsible for the publishing of content that you
+        post.
+      </p>
+      <h2>Changes</h2>
+      <p>
+        We reserve the right, at our sole discretion, to modify or replace these
+        Terms at any time. If a revision is material we will try to provide at
+        least 30 days' notice prior to any new terms taking effect. What
+        constitutes a material change will be determined at our sole discretion.
+      </p>
+      <h2>Contact Us</h2>
+      <p>If you have any questions about these Terms, please contact us.</p>
+    </div>
+  );
+}
